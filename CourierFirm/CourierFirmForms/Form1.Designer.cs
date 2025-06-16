0031f1aa858de5966a1887bca1d263e7d7e03d3f
tedi-nedkova <teodora_nedkova@abv.bg>
2025-06-16 14:58:52 +0300

    windows form project added

---
+++ CourierFirm/CourierFirmForms/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CourierFirmForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(49, 163, 110);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1083, 137);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(49, 163, 110);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 549);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1083, 86);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(129, 227, 170);
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 137);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(100, 412);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(129, 227, 170);
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(981, 137);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(102, 412);
+            panel4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Book Antiqua", 64.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(281, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(546, 129);
+            label1.TabIndex = 4;
+            label1.Text = "Welcome!";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(341, 375);
+            button1.Name = "button1";
+            button1.Size = new Size(416, 92);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(192, 255, 192);
+            label2.Font = new Font("Impact", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(418, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(244, 75);
+            label2.TabIndex = 5;
+            label2.Text = "NexDrop";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+            BackColor = Color.White;
+            ClientSize = new Size(1083, 635);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+    }
+}
